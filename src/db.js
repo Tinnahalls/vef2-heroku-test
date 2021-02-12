1b66d402fc32bac2d71f1092ca7e4aba0689e700
Tinnahalls <tinnahalls@gmail.com>
2021-02-12 22:55:52 +0000

    added to db SSL connection fix

--- src/db.js
+++ src/db.js
@@ -1,18 +1,24 @@
+
 import pg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 
-
 const {
-  DATABASE_URL: connectionString
+  DATABASE_URL: connectionString,
+  NODE_ENV: nodeEnv = 'development',
 } = process.env;
 
+console.log('process.env :>> ', process.env.DATABASE_URL);
+
 if (!connectionString) {
-  console.error("Missing DATABASE_URL");
+  console.error('Vantar DATABASE_URL!');
   process.exit(1);
 }
 
-const pool = new pg.Pool({ connectionString });
+// Notum SSL tengingu við gagnagrunn ef við erum *ekki* í development mode, þ.e.a.s. á local vél
+const ssl = nodeEnv !== 'development' ? { rejectUnauthorized: false } : false;
+
+const pool = new pg.Pool({ connectionString, ssl });
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
@@ -21,20 +27,13 @@ pool.on('error', (err) => {
 
 export async function query(q, values = []) {
   const client = await pool.connect();
-  
+
   try {
     const result = await client.query(q, values);
     return result;
-
-  } catch (e) {
+  } catch(e) {
     console.error('Error selecting', e);
   } finally {
     client.release();
   }
 }
-  
- 
-
-
-
-
