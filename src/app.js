43c0f00db2927ecc9a816ce2d8e3c8ba8cb26086
Tinnahalls <tinnahalls@gmail.com>
2021-02-13 23:01:11 +0000

    Tried to add express layout with EJS but could not do it with ES modules

--- src/app.js
+++ src/app.js
@@ -9,18 +9,27 @@ Birtir innslegin gögn ásamt heiti á skrá.
 */
 
 import express from 'express';
-
-import { query } from './db.js'
+import { query } from './db.js';
 
 import { body, validationResult } from 'express-validator';
 
-
-const {
-  PORT: port = 3000
-} = process.env;
-
-
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+//express EJS
+//app.locals.importantize = str => `${str}!`;
+
+//const viewsPath = new URL('./views');
+
+//app.set('view engine', 'ejs');
+//app.set('views', viewsPath);
+
+//app.get('/', (req, res) => {
+  // `title` verður aðgengilegt sem breyta í template
+  //res.render('index', { title: 'Forsíða' });
+//});
+
 
 // Kemur í veg fyrir .on() dótið sem við gerðum í fyrri dæmum
 
@@ -38,39 +47,45 @@ app.get('/', async (req, res,) => {
   const comment = '';
 
   res.send(`
-  Nafnalisti:
-  <form method="post" action="/post">
-  <label>
-    Nafn:
-    <input required type="text" name="name" value="${names}">
-  </label>
-<label>
-  Kennitala:
-  <input
-    required
-    type="text"
-    pattern="${nationalIdPattern}"
-    name="nationalId"
-    value="${nationalId}">
-</label>
-<label>
-Athugasmend:
-<input required type="text" name="comment" value="${comment}">
-</label>
-  <button>Senda</button>
-</form>
+  <body>
+
+  <h1> Nafnalisti: </h1>
+  <div class="column">
+    <div class="row">
+      <form method="post" action="/post">
+      <label>
+      Nafn:
+     <input required type="text" name="name" value="${names}">
+       </label>
+      <label>
+       Kennitala:
+       <input
+        required
+       type="text"
+       pattern="${nationalIdPattern}"
+       name="nationalId"
+       value="${nationalId}">
+    </label>
+    <label>
+    Athugasmend:
+    <input required type="text" name="comment" value="${comment}">
+    </label>
+    <button>Senda</button>
+    </form>
+  </div
+ </div>
+ </body>
   `) ;
 });
 
 
-app.use(express.urlencoded({ extended: true }));
 
 //missing error catching!
 app.post('/post', async (req, res) => {
   const name = req.body.name;
   console.log('name :>>', name);
 
-  const nationalId = req.body.nationalId;
+  const nationalId = req.body.nationalid;
   console.log('nationalId :>>', nationalId);
 
   const comment = req.body.comment;
@@ -83,6 +98,12 @@ app.post('/post', async (req, res) => {
   res.redirect('/');
 });
 
+
+
+
+const {
+  PORT: port = 3000
+} = process.env;
 
 
 app.listen(port, () => {
